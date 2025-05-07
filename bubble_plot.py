15d1618581635a806bd0ce8e07c6628ad8061a03
53-97 <abhishek.negi53@gmail.com>
2025-05-07 22:38:08 +0200

    Delete bubble_plot.py

--- bubble_plot.py
+++
@@ -1,7 +0,0 @@
-import plotly.express as px
-df = px.read_csv('C:\Users\ABHISHEK\Desktop\Tools & Methods of Data Analysis\population_current.csv')
-
-fig = px.scatter(df.query("year==2023"), x="gdpPercap", y="lifeExp",
-	         size="pop", color="continent",
-                 hover_name="country", log_x=True, size_max=60)
-fig.show()
