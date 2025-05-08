381ce5a988dbabc49e95ea4ed741ee72ebc787e0
53-97 <abhishek.negi53@gmail.com>
2025-05-08 02:11:29 +0200

    Update 01_bubble_plot.py

--- 01_bubble_plot.py
+++ 01_bubble_plot.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+# Load the data set
 df = px.read_csv('C:\Users\ABHISHEK\Desktop\Tools & Methods of Data Analysis\population_current.csv')
 
 fig = px.scatter(df.query("year==2023"), x="gdpPercap", y="lifeExp",
